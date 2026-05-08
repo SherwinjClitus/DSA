c79d639a2e4e22c1145164e465fbc28711bec3ea
Sherwin <126876512+SherwinjClitus@users.noreply.github.com>
2026-05-08 22:22:17 +0530

    Create 2481-StrictlyPalindromicNumber

---
+++ https:/github.com/SherwinjClitus/DSA/2481-StrictlyPalindromicNumber/2481-StrictlyPalindromicNumber.java
@@ -0,0 +1,8 @@
+// Last updated: 08/05/2026, 22:22:17
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        if(n<4) return true;
+        else return false;
+        
+    }
+}
